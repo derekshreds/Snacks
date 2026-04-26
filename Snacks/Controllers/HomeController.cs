0e13a91e1aa8319d8cd3821c0869d7410c58bc5d
Derek Morris <derekshreds@gmail.com>
2026-04-26 13:44:00 -0500

    bump versions

--- Snacks/Controllers/HomeController.cs
+++ Snacks/Controllers/HomeController.cs
@@ -42,7 +42,7 @@ public sealed class HomeController : Controller
     {
         status    = "healthy",
         timestamp = DateTime.UtcNow,
-        version   = "2.5.0",
+        version   = "2.5.1",
     });
 
     /// <summary>
